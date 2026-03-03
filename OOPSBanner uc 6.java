ba44bfea2d6d3245f00c3f60573810f37b421587
sk9602-byte <sk9602@srmist.edu.in>
2026-03-03 15:04:42 +0530

    Add files via upload

---
+++ OOPSBanner uc 6.java
@@ -0,0 +1,57 @@
+public class OOPSBannerApp {
+
+    
+    public static String[] getOPattern() {
+        return new String[]{
+                String.join("", "  ***  "),
+                String.join("", " *   * "),
+                String.join("", "*     *"),
+                String.join("", "*     *"),
+                String.join("", "*     *"),
+                String.join("", " *   * "),
+                String.join("", "  ***  ")
+        };
+    }
+
+    
+    public static String[] getPPattern() {
+        return new String[]{
+                String.join("", " ****  "),
+                String.join("", " *   * "),
+                String.join("", " *   * "),
+                String.join("", " ****  "),
+                String.join("", " *     "),
+                String.join("", " *     "),
+                String.join("", " *     ")
+        };
+    }
+
+    
+    public static String[] getSPattern() {
+        return new String[]{
+                String.join("", "  **** "),
+                String.join("", " *     "),
+                String.join("", " *     "),
+                String.join("", "  ***  "),
+                String.join("", "     * "),
+                String.join("", "     * "),
+                String.join("", " ****  ")
+        };
+    }
+
+    
+    public static void main(String[] args) {
+
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + "  "
+                    + oPattern[i] + "  "
+                    + pPattern[i] + "  "
+                    + sPattern[i]);
+        }
+    }
+}
